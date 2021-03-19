edf4c42947f9e0202a3bbe8bdd36f7309c8b3357
Francisco <fjavier86+github@gmail.com>
2021-03-18 21:50:14 -0600

    feat(filter): implement filter and new item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -20,7 +21,8 @@ import { TodoComponent } from './page/todo/todo.component';
     AppRoutingModule,
     NgxSpinnerModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/page/todo/todo.component.ts
+++ src/app/page/todo/todo.component.ts
@@ -1,7 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { max } from 'rxjs/operators';
 import { ApiService } from 'src/app/services/api/api.service';
 import { UiService } from 'src/app/services/ui/ui.service';
 
+interface Todos {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
@@ -9,9 +16,9 @@ import { UiService } from 'src/app/services/ui/ui.service';
 })
 export class TodoComponent implements OnInit {
 
-  data: any[];
-  copyData: any[];
-  filterSearch: string;
+  data: Todos[];
+  copyData: Todos[];
+  newItem = '';
 
   constructor(
     private api: ApiService,
@@ -40,19 +47,18 @@ export class TodoComponent implements OnInit {
       case 'pending':
         this.data = this.copyData.filter(todo => todo.completed === false);
         break;
-
       default:
         this.data = this.copyData;
         break;
     }
   }
   uncomplete(index): void {
-    this.data[index]['completed'] = false;
-    this.copyData[index]['completed'] = false;
+    this.data[index].completed = false;
+    this.copyData[index].completed = false;
   }
   complete(index): void {
-    this.data[index]['completed'] = true;
-    this.copyData[index]['completed'] = true;
+    this.data[index].completed = true;
+    this.copyData[index].completed = true;
   }
 
   delete(index): void {
@@ -60,7 +66,28 @@ export class TodoComponent implements OnInit {
     this.copyData.splice(index, 1);
   }
 
-  search(e, v): void {
-    console.log(this.filterSearch);
+  search(e): void {
+    if (e.length > 0) {
+      this.data = this.data.filter(element => element.title.toLowerCase().indexOf(e.toLowerCase()) > -1);
+    } else {
+      this.data = this.copyData;
+    }
+  }
+
+  addNewItem() {
+    let newTodo: Todos;
+    if (this.newItem !== '') {
+      let maxId = Math.max(...this.data.map(element => element.id));
+      let maxUserId = Math.max(...this.data.map(element => element.userId));
+      newTodo = {
+        userId: maxUserId++,
+        id: maxId++,
+        title: this.newItem,
+        completed: false
+      }
+      this.data.push(newTodo);
+      this.copyData.push(newTodo);
+      this.newItem = '';
+    }
   }
 }
